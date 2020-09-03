e12e8edbd9d5c7ec428723659254c49f5583fad9
Martynice <marticiy@ukr.net>
2020-09-03 16:49:23 +0300

    added classes

---
+++ src/main/java/com/internet/shop/Main.java
@@ -0,0 +1,28 @@
+package com.internet.shop;
+
+import com.internet.shop.db.Storage;
+import com.internet.shop.lib.Injector;
+import com.internet.shop.model.Product;
+import com.internet.shop.service.ProductService;
+
+public class Main {
+    private static Injector injector = Injector.getInstance("com.internet.shop");
+
+    public static void main(String[] args) {
+        ProductService productService = (ProductService) injector.getInstance(ProductService.class);
+        Product iphone = new Product("IPhone", 1500.0);
+        Product samsung = new Product("Samsung", 1500.0);
+        Product appleWatch = new Product("Apple Watch", 1000.0);
+        Product asus = new Product("Asus", 5000.0);
+        productService.create(iphone);
+        productService.create(samsung);
+        productService.create(appleWatch);
+        productService.create(asus);
+        Storage.products.forEach(System.out::println);
+        asus.setPrice(asus.getPrice() * 2);
+        productService.update(asus);
+        Storage.products.forEach(System.out::println);
+        productService.delete(appleWatch.getId());
+        Storage.products.forEach(System.out::println);
+    }
+}
---
+++ src/main/java/com/internet/shop/model/Product.java
@@ -0,0 +1,47 @@
+package com.internet.shop.model;
+
+public class Product {
+    private Long id;
+    private String name;
+    private Double price;
+
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product(Product product) {
+        this.id = product.getId();
+        this.name = product.getName();
+        this.price = product.getPrice();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "id=" + id + ", name='" + name + '\'' + ", price=" + price + '}';
+    }
+}
