811b17353a6aa9fb811be6b402828db48e1add6b
Martynice <marticiy@ukr.net>
2020-09-07 19:19:29 +0300

    second part completed

---
+++ src/main/java/com/internet/shop/dao/impl/ShoppingCartDaoImpl.java
@@ -0,0 +1,51 @@
+package com.internet.shop.dao.impl;
+
+import com.internet.shop.dao.ShoppingCartDao;
+import com.internet.shop.db.Storage;
+import com.internet.shop.lib.Dao;
+import com.internet.shop.model.ShoppingCart;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@Dao
+public class ShoppingCartDaoImpl implements ShoppingCartDao {
+    @Override
+    public ShoppingCart create(ShoppingCart shoppingCart) {
+        Storage.addShoppingCart(shoppingCart);
+        return shoppingCart;
+    }
+
+    @Override
+    public Optional<ShoppingCart> get(Long id) {
+        return Storage.shoppingCarts.stream()
+                .filter(x -> x.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public Optional<ShoppingCart> getByUser(Long userId) {
+        return Storage.shoppingCarts.stream()
+                .filter(x -> x.getUserId().equals(userId))
+                .findFirst();
+    }
+
+    @Override
+    public List<ShoppingCart> getAll() {
+        return Storage.shoppingCarts;
+    }
+
+    @Override
+    public ShoppingCart update(ShoppingCart shoppingCart) {
+        IntStream.range(0, Storage.shoppingCarts.size())
+                .filter(i -> Storage.shoppingCarts.get(i).getId().equals(shoppingCart.getId()))
+                .forEach(i -> Storage.shoppingCarts.set(i, shoppingCart));
+        return shoppingCart;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return Storage.shoppingCarts
+                .removeIf(x -> x.getId().equals(id));
+    }
+}
