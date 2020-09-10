df65d69f27e670b7e904d35c83ee532bfbd4dcd3
Martynice <marticiy@ukr.net>
2020-09-11 00:03:23 +0300

    added order controllers

---
+++ src/main/java/com/internet/shop/controllers/order/GetAllOrdersController.java
@@ -0,0 +1,24 @@
+package com.internet.shop.controllers.order;
+
+import com.internet.shop.lib.Injector;
+import com.internet.shop.service.OrderService;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/order/all")
+public class GetAllOrdersController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("com.internet.shop");
+    private final OrderService orderService =
+            (OrderService) injector.getInstance(OrderService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.setAttribute("orders", orderService.getAll());
+        req.getRequestDispatcher("/WEB-INF/views/order/all.jsp").forward(req, resp);
+    }
+}
--- src/main/webapp/WEB-INF/views/order/details.jsp
+++ src/main/webapp/WEB-INF/views/order/details.jsp
@@ -2,7 +2,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Shopping Cart</title>
+    <title>Details</title>
 </head>
 <body>
 <%@include file="../header.jsp"%>
@@ -12,7 +12,7 @@
         <th>Name</th>
         <th>Price</th>
     </tr>
-    <c:forEach var = "product" items = "${products}">
+    <c:forEach var = "product" items = "${orderProducts}">
         <tr>
             <td>
                 <c:out value = "${product.id}"/>
@@ -23,12 +23,8 @@
             <td>
                 <c:out value = "${product.price}"/>
             </td>
-            <td>
-                <a href="${pageContext.request.contextPath}/shopping-cart/product/delete?id=${product.id}">Delete</a>
-            </td>
         </tr>
     </c:forEach>
 </table>
-<a href="${pageContext.request.contextPath}/order/complete?cartId=${cartId}"><button>Order</button></a>
 </body>
 </html>
