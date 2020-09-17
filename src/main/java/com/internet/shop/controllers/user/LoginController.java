316ebce796aea807fbb687bae06ed302f0d5f016
Martynice <marticiy@ukr.net>
2020-09-17 11:53:01 +0300

    cleaned doGet method in login controller

--- src/main/java/com/internet/shop/controllers/user/LoginController.java
+++ src/main/java/com/internet/shop/controllers/user/LoginController.java
@@ -24,11 +24,6 @@ public class LoginController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        if (userService.findByLogin("admin").isPresent()) {
-            req.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(req, resp);
-        } else {
-            resp.sendRedirect(req.getContextPath() + "/inject");
-        }
     }
 
     @Override
